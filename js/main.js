0672319245995d24100a541bda9ed93a4471fd59
premshravan <145581914+premshravan@users.noreply.github.com>
2023-09-29 00:53:14 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function show(){
+    var ways = document.querySelector(".ways"); 
+if (ways.style.display === "none") {
+    ways.style.display = "block";
+} else {
+    ways.style.display = "none";
+}
+}
